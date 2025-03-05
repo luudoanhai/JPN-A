2d7e30c3eac5ca8c0f8c0b9f0428a25c0f38a1fc
luudoanhai <hluudoan@gmail.com>
2025-03-05 22:55:45 +0700

    Create b8.js

---
+++ ss21/baitap/b8.js
@@ -0,0 +1,7 @@
+for(let i = 100;i < 1000 ; i++);
+let a = Math.floor(i/100);
+let b = Math.floor((i%100)/10);
+let c = i % 10;
+  if(Math.pow(a,3) + Math.pow(b,3) + Math.pow(c,3)==i){
+    console.log("Day la so can tim");
+  }
